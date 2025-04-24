f9eb396a258ae6a8860b8265024ac0a42b51471b
Jon Cherry <sportsboy12@gmail.com>
2025-04-23 19:17:18 -0700

    add first pages

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
-  <h1>Goodbye Cruel World!</h1>
-  <p>Welcome to your Electron application.</p>
+  <v-container>
+    <PageNavigator v-if="keyValid"></PageNavigator>
+    <Welcome v-else></Welcome>
+  </v-container>
 </template>
 
 <script setup>
+import PageNavigator from './pages/PageNavigator.vue'
+import Welcome from './pages/Welcome.vue'
+
+var keyValid = false;
 console.log('👋 This message is being logged by "App.vue", included via Vite');
 </script>
